2d236f720da40e6cbf9c2ac738071a87b10b39b3
tpreston96 <tpreston96@gmail.com>
2021-02-19 17:59:30 -0600

    geolocation API w failure callback

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    // success callback
+    (position) => console.log(position),
+    // failure callback
+    (err) => console.log(err)
+  );
   return <div>Hi there!</div>;
 };
 
