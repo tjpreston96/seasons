93be6991752b46955d689a9e11f4ce60bb7a0530
tpreston96 <tpreston96@gmail.com>
2021-02-19 19:09:36 -0600

    error message

--- src/index.js
+++ src/index.js
@@ -7,20 +7,29 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
-    
+    this.state = { lat: null, errorMessage: "" };
+
     window.navigator.geolocation.getCurrentPosition(
       // success callback
       (position) => {
-        this.setState({ lat: position.coords.latitude})
+        console.log(position);
+        this.setState({ lat: position.coords.latitude });
       },
       // failure callback
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
