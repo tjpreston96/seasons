1577a846aa9d2faa666adddcc70cf58c4132dfa9
tpreston96 <tpreston96@gmail.com>
2021-02-19 18:49:21 -0600

    Update state property

--- src/index.js
+++ src/index.js
@@ -7,18 +7,20 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {lat: null };
-  }
-
-  render() {
+    this.state = { lat: null };
+    
     window.navigator.geolocation.getCurrentPosition(
       // success callback
-      (position) => console.log(position),
+      (position) => {
+        this.setState({ lat: position.coords.latitude})
+      },
       // failure callback
       (err) => console.log(err)
     );
+  }
 
-    return <div>Latitude: </div>;
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
