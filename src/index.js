e39e1c5fa25959db560d5a5b0a084acf862876cb
tpreston96 <tpreston96@gmail.com>
2021-02-19 18:40:27 -0600

    adding constructor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+// class App extends React.Component
 class App extends Component {
-  state = {};
+  // state = {};
+  constructor(props) {
+    super(props);
+
+    this.state = {lat: null };
+  }
+
   render() {
     window.navigator.geolocation.getCurrentPosition(
       // success callback
@@ -10,9 +17,9 @@ class App extends Component {
       // failure callback
       (err) => console.log(err)
     );
+
     return <div>Latitude: </div>;
   }
 }
-
 
 ReactDOM.render(<App />, document.querySelector("#root"));
