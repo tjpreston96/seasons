60b245698a86bb66138d124c74fd74ac741cc1ce
tpreston96 <tpreston96@gmail.com>
2021-02-19 18:19:46 -0600

    refactor functional component to class component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { Component } from 'react';
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    // success callback
-    (position) => console.log(position),
-    // failure callback
-    (err) => console.log(err)
-  );
-  return <div>Hi there!</div>;
-};
+class App extends Component {
+  state = {};
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      // success callback
+      (position) => console.log(position),
+      // failure callback
+      (err) => console.log(err)
+    );
+    return <div>Latitude: </div>;
+  }
+}
+
 
 ReactDOM.render(<App />, document.querySelector("#root"));
